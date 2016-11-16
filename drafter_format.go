b54935e5ca7daa21d61ec684cc260980e1bd0163
dameleon <dameleon@gmail.com>
2016-11-17 00:03:57 +0900

    implement DrafterFormat

---
+++ drafter_format.go
@@ -0,0 +1,19 @@
+package drafter
+
+type DrafterFormat int
+
+const (
+	DRAFTER_SERIALIZE_YAML DrafterFormat = iota
+	DRAFTER_SERIALIZE_JSON
+)
+
+func (df DrafterFormat) String() string {
+	switch df {
+	case DRAFTER_SERIALIZE_YAML:
+		return "drafter_serialize_yaml"
+	case DRAFTER_SERIALIZE_JSON:
+		return "drafter_serialize_json"
+	default:
+		return "unknown"
+	}
+}
